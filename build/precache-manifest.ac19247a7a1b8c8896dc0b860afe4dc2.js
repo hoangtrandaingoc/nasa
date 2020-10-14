4dae97a957a78ed8b9ddf92556d7a831980140d3
Wayne <hoangtrandaingoc@gmail.com>
2020-10-14 20:08:30 +0700

    update

---
+++ build/precache-manifest.ac19247a7a1b8c8896dc0b860afe4dc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694857280ac5b45387f84cf7a8451c6e",
+    "url": "/nasa/index.html"
+  },
+  {
+    "revision": "8d0c230617bf13191e48",
+    "url": "/nasa/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0f0a07093954b72fa215",
+    "url": "/nasa/static/css/main.c9bc7cd0.chunk.css"
+  },
+  {
+    "revision": "8d0c230617bf13191e48",
+    "url": "/nasa/static/js/2.baf27c51.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/nasa/static/js/2.baf27c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0a07093954b72fa215",
+    "url": "/nasa/static/js/main.e32f7a91.chunk.js"
+  },
+  {
+    "revision": "fbe304cb49a4f163cea2",
+    "url": "/nasa/static/js/runtime-main.b7f74675.js"
+  }
+]);
